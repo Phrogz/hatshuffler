b5f47a2b9e903baab96fa692c5e5bfad9334c624
Gavin Kistner <gavin@phrogz.net>
2019-04-08 09:08:45 -0600

    Robustly handle CSV saved as UTF8 with BOM

--- setup.js
+++ setup.js
@@ -4,8 +4,9 @@ const csv = require('csv-parse/lib/sync')
 var fs = require('fs')
 
 function defaultSeason(config) {
-	const playercsv = fs.readFileSync(config.players).toString()
-	const players = csv(playercsv, {columns:true, delimiter:','}).map(p => Player.fromGRUCSV(p))
+	// \ufeff is the UTF8 BOM; the csv library barfs on it, so it needs to be removed, if present.
+	const playercsv = fs.readFileSync(config.players).toString().replace(/^\ufeff/, '')
+	const players = csv(playercsv, {columns:true, delimiter:',', ltrim:true, rtrim:true}).map(p => Player.fromGRUCSV(p))
 	if (config.season) {
 		return Season.fromCSV(fs.readFileSync(config.season).toString(), players)
 	} else {

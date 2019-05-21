e66a87014f28d85d4af663564f70aadb1405b7e0
Gavin Kistner <gavin@phrogz.net>
2019-05-20 22:40:04 -0600

    WIP: baggage setup, working on swapping

--- setup.js
+++ setup.js
@@ -7,6 +7,24 @@ function defaultSeason(config) {
 	// \ufeff is the UTF8 BOM; the csv library barfs on it, so it needs to be removed, if present.
 	const playercsv = fs.readFileSync(config.players).toString().replace(/^\ufeff/, '')
 	const players = csv(playercsv, {columns:true, delimiter:',', ltrim:true, rtrim:true}).map(p => Player.fromGRUCSV(p))
+	const playersByName = new Map(players.map(p => [p.name, p]))
+	players.filter(p => p.baggageName).forEach(p => {
+		const other = playersByName.get(p.baggageName);
+		if (other) {
+			if (other.baggageName) {
+				if (other.baggageName===p.name) {
+					p.baggage = other
+					other.baggage = p
+				} else {
+					console.warn(`"${p.name}" asked to baggage with "${p.baggageName}", but they asked to baggage with "${other.baggageName}".`)
+				}
+			} else {
+				console.warn(`"${p.name}" asked to baggage with "${p.baggageName}", but they did not select any baggage.`)
+			}
+		} else {
+			console.warn(`"${p.name}" asked to baggage with "${p.baggageName}", but I don't know who that is.`)
+		}
+	});
 	if (config.season) {
 		return Season.fromCSV(fs.readFileSync(config.season).toString(), players)
 	} else {
